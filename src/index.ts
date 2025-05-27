3a105c47a63fd40d92efaa1cf7556e6227cc220b
knownout <a.slavinskii@icloud.com>
2025-05-27 18:50:10 +0300

    Add mongo controller in the default export

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
+import MongoController from "~/mongo-controller"
 import MongoPlugin, { MongoPluginConfig } from "~/mongo-plugin"
 
 export {
   MongoPlugin,
+  MongoController,
   type MongoPluginConfig
 }
